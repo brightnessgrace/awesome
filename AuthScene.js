3e181fcba03f7b96419390c50cc3e941f9a1e72b
brightnessgrace <gracebrightness@gmail.com>
2021-06-07 17:18:32 +0300

    this is the next screen after the loading screen

---
+++ AuthScene.js
@@ -0,0 +1,26 @@
+
+
+import React , { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+ class AuthScene extends Component{
+
+render ()  {
+
+
+    return (
+          
+     <View>   
+     
+           <Text>
+
+                  helo
+        </Text>
+
+        </View>
+
+    )
+}
+
+}
